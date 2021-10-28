5147e0059d949cf1b91e757b2862213eeeac6265
VladeCV <vladimircabrecabri@gmail.com>
2021-10-27 23:49:17 -0400

    Auth Google

---
+++ firebase.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCnlx9_6HV_B4C9LVXaqIqsFxViQaWF0Do",
+  authDomain: "insta-cl-334f7.firebaseapp.com",
+  projectId: "insta-cl-334f7",
+  storageBucket: "insta-cl-334f7.appspot.com",
+  messagingSenderId: "597052811641",
+  appId: "1:597052811641:web:ca797827e0966dc3afe7a8",
+  measurementId: "G-2RWP2HGK55",
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore();
+const storage = getStorage();
+const analytics = getAnalytics(app);
+
+export { app, db, storage };
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Header from "../components/Header";
 import Feed from "../components/Feed";
-
+import Modal from "../components/Modal";
 export default function Home() {
   return (
     <div className="bg-gray-50 h-screen overflow-y-scroll scrollbar-hide">
@@ -15,6 +15,7 @@ export default function Home() {
       {/*Feed*/}
       <Feed />
       {/*Modal*/}
+      <Modal />
     </div>
   );
 }
